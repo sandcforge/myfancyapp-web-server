af502b7b8963a6bfd16648e809bca4ba69a4b388
sheng <sheng@shengs-MacBook-Pro.local>
2020-02-02 16:55:57 -0800

    inital checkin

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const os = require('os');
-
+const http = require('http');
+const https = require('https');
+const fs = require('fs');
 const app = express();
 
 app.use(express.static('dist'));
-app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
+app.get('/api/getUsername', (req, res) => {
+    res.send({ username: req.headers.host, appname: 'appname' });
+});
 
-app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
+app.get('/api/subdomain', (req, res) => {
+    console.log(req.headers['x-forwarded-host']);
+    res.send(req.headers['x-forwarded-host'] + '  : ' + String(Date.now()));
+});
+app.get('/api/host', (req, res) => {
+    console.log(req.headers['host']);
+    res.send(req.headers['host'] + '  : ' + String(Date.now()));
+});
+
+http.createServer(app).listen(process.env.PORT || 8080, () => {
+    console.log(`HTTP server is now running on port: ${process.env.PORT || 8080}`);
+  });
