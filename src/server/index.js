ebd024c27291a3eccb1cc20b1700763c6dc6edaf
sheng <sheng@shengs-MacBook-Pro.local>
2020-02-05 09:35:49 -0800

    support eb cli deploy

--- src/server/index.js
+++ src/server/index.js
@@ -1,25 +1,33 @@
 const express = require('express');
 const os = require('os');
 const http = require('http');
-const https = require('https');
 const fs = require('fs');
 const app = express();
 
 app.use(express.static('dist'));
+
 app.get('/api/getUsername', (req, res) => {
     res.send({ username: req.headers.host, appname: 'appname' });
 });
 
-app.get('/api/subdomain', (req, res) => {
-    console.log(req.headers['x-forwarded-host']);
-    res.send(req.headers['x-forwarded-host'] + '  : ' + String(Date.now()));
-});
+// Get subdomain name
 app.get('/api/host', (req, res) => {
     console.log(req.headers['host']);
     res.send(req.headers['host'] + '  : ' + String(Date.now()));
 });
 
+// Test endpoint.
+app.get('/api/now', (req, res) => {
+    res.send(Date.now().toString());
+});
+
+// ELB health check endpoint. Have to return 200, which is consistent with the ELB config.
+app.get('/api/ping', (req, res) => {
+    res.send(200);
+});
+
 const httpPort = process.env.PORT || 8080;
+
 http.createServer(app).listen(httpPort, () => {
 console.log(`HTTP server is now running on port: ${httpPort}`);
 });
