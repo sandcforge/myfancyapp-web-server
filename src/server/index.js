8a092a177b6a1ec1fb3b275f36eec988d08b54d6
sheng <sheng@shengs-MacBook-Pro.local>
2020-02-06 16:21:02 -0800

    add support to aws rds mysql

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const http = require('http');
 const winston = require('winston');
+const mysql = require('mysql');
 
-const app = express();
 const logger = winston.createLogger({
   level: 'info',
   format: winston.format.combine(
@@ -16,6 +16,7 @@ const logger = winston.createLogger({
   ]
 });
 
+const app = express();
 app.use(express.static('dist'));
 
 app.get('/api/getUsername', (req, res) => {
@@ -38,9 +39,32 @@ app.get('/api/ebhealthcheck', (req, res) => {
   res.sendStatus(200);
 });
 
+// ELB health check endpoint. Have to return status of 200, which is consistent with the ELB config.
+app.get('/api/db', (req, res) => {
+  dbConnection.query('SELECT * FROM users WHERE createAt > 202023423', (err, result, fields) => {
+    if (err) throw err;
+    res.send(result);
+  });
+});
+
 // 'process.env.PORT' is a MUST if deployed in EB.
 const httpPort = process.env.PORT || 8080;
-
 http.createServer(app).listen(httpPort, () => {
   logger.info(`HTTP server is now running on port: ${httpPort}`);
+});
+
+const dbConnection = mysql.createConnection({
+  host: 'test-database.cjn9z9zzyyxs.us-east-1.rds.amazonaws.com',
+  user: 'myfancyapp',
+  password: 'myfancyapp',
+  database: 'myfancyapp',
+  port: 3306,
+});
+
+dbConnection.connect((err) => {
+  if (err) {
+    logger.error(`Database connection failed: ${err.stack}`);
+    return;
+  }
+  logger.info('Connected to database.');
 });
